d98bc83f32d8e4ede869fd1b79194e494f5ff933
IgorVeshkin <IgorVeshkin.official@yandex.ru>
2026-04-17 23:04:48 +0300

    Added routing in App.tsx. Created LoginPage with useReducer logic

--- chat_client/src/App.tsx
+++ chat_client/src/App.tsx
@@ -1,160 +1,20 @@
-import { useEffect, useRef, useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-import './App.css'
+// Импорт страниц системы
+import ChatroomPage from "./pages/ChatroomPage";
+import LoginPage from "./pages/LoginPage";
 
-import useFetchData from './hooks/useFetchData';
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/* Страница чата */}
+                <Route path="*" element={<ChatroomPage/>} />
 
-interface MessageInputFormProps {
-  message: string,
-  setMessage: React.Dispatch<React.SetStateAction<string>>,
-  setMessageList: React.Dispatch<React.SetStateAction<string[]>>,
-  textfieldID: string,
-  webSocket: WebSocket | null,
+                {/* Страница логина */}
+                <Route path="login" element={<LoginPage/>} />
+
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-const MessageInputForm: React.FC<MessageInputFormProps> = ({ message, setMessage, setMessageList, textfieldID, webSocket }) => {
-
-  const handleMessageInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-
-    setMessage(e.target.value);
-
-  }
-
-  const handleMessageSubmit = (e: React.SubmitEvent) => {
-    e.preventDefault()
-
-    if (webSocket && message.trim()) {
-
-      webSocket.send(message)
-      setMessageList(prev => [...prev, message])
-      setMessage("")
-      
-    }
-    
-  }
-
-  const handleFormRefresh = (e: React.MouseEvent<HTMLButtonElement>) => {
-
-    e.preventDefault();
-
-    setMessage("");
-
-  }
-
-  return (<form onSubmit={handleMessageSubmit} ><input 
-            id={textfieldID}
-            value={message}
-            onChange={handleMessageInput}
-            placeholder='Enter message....'
-  />
-
-  <button type="submit">Send</button>
-  <button onClick={handleFormRefresh}>Reset</button>
-  </form>)
-
-}
-
-function App() {
-
-  const [messageList, setMessageList] = useState<string[]>([])
-  const [message, setMessage] = useState<string>("")
-  
-
-  // Кастомный хук для получении данных текущего чата
-  interface Chatroom {
-    chatroom: {
-      uuid: string,
-      title: string,
-      coverImage: string,
-      users_list: Array<Object>,
-      created_by: Object,
-    }
-  }
-
-  const chatroomURL = useRef<string>("http://127.0.0.1:8000/api/chatroom/")
-
-  const { data: chatroomData, loading: chatroomLoading, error: chatroomError } = useFetchData<Chatroom>(chatroomURL.current)
-
-  // Websocket
-  var websocketServerURL = "ws://localhost:8000/ws/testing/?chatroom_uuid="
-
-  const socketRef = useRef<WebSocket | null>(null)
-
-
-  // Работа websocket
-  useEffect(() => {
-
-    // Данные uuid чата не еще не получены
-    if (!chatroomData?.chatroom?.uuid) {
-      return;
-    }
-
-    const websocketServerURLWithParams = `${websocketServerURL}${chatroomData?.chatroom.uuid}`
-
-    const socketInstance: WebSocket | null = new WebSocket(websocketServerURLWithParams)
-
-    socketRef.current = socketInstance;
-
-    socketInstance.onopen = () => {
-      console.log('Client has been connected....');
-    };
-
-    socketInstance.onmessage = (event) => {
-
-      setMessageList(prev => [...prev, event.data])
-
-    }
-
-    socketInstance.onclose = () => {
-      console.log('Client has been disconnected');
-    };
-
-    socketInstance.onerror = (error) => {
-      console.error('WebSocket error:', error);
-    };
-
-    return(() => {
-
-      socketInstance.close()
-
-    })
-
-  }, [chatroomData])
-
-  return (
-    <>
-      <section>
-        <h2>If you see this message then everything works fine</h2>
-        {!chatroomLoading &&
-        
-          <section>
-            <h2>{ chatroomData?.chatroom?.title }</h2>
-            {/* <img src={`http://127.0.0.1:8000${ chatroomData?.chatroom?.coverImage }`} /> */}
-          </section>
-
-        }
-
-        {chatroomError && 
-          <h3 style={{ color: 'red', }}>Ошибка получения данных чата: { chatroomError } </h3>
-        }
-        
-
-        {
-          messageList?.map((msg: string) => (
-            <h3>{msg}</h3>
-          ))
-        }
-      </section>
-
-      <MessageInputForm 
-        message={message} 
-        setMessage={setMessage} 
-        setMessageList={setMessageList}
-        textfieldID="messageTextInput" 
-        webSocket={socketRef.current}
-        />
-    </>
-  )
-}
-
-export default App
---
+++ chat_client/src/pages/LoginPage.tsx
@@ -0,0 +1,80 @@
+import { useReducer } from 'react'
+
+
+interface LoginState {
+   username: string,
+   password: string
+}
+
+
+const initialState: LoginState = {
+    username: "",
+    password: ""
+}
+
+const loginStateTypes = {
+    SET_USERNAME: "SET_USERNAME",
+    SET_PASSWORD: "SET_PASSWORD",
+    RESET: "RESET"
+    
+} as const
+
+type Action = {
+    
+    type: typeof loginStateTypes.SET_USERNAME;
+    payload: string;
+
+} | {
+
+    type: typeof loginStateTypes.SET_PASSWORD;
+    payload: string;
+
+} | {
+
+    type: typeof loginStateTypes.RESET;
+
+};
+
+const loginPageReducer = (state: LoginState, action: Action) : LoginState => {
+    
+    switch(action.type) {
+        case loginStateTypes.SET_USERNAME:
+            return {...state, username: action.payload}
+        case loginStateTypes.SET_PASSWORD:
+            return {...state, password: action.payload}
+        case loginStateTypes.RESET:
+            return {username: "", password: "",}
+        default:
+            return state
+
+    }
+
+}
+
+function LoginPage() {
+
+    const [authData, authDataDispatcher] = useReducer(loginPageReducer, initialState)
+
+
+    const handleSubmit = (e: React.SubmitEvent) => {
+
+        e.preventDefault();
+
+        console.log("Username: " + authData.username + "\n" + "Password: " + authData.password)
+
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Вход в профиль</h2>
+            <input type="text" placeholder='Username' value={authData.username} onChange={e => authDataDispatcher({ type: loginStateTypes.SET_USERNAME, payload: e.target.value })} />
+            <input type="password" placeholder='Password' value={authData.password} onChange={e => authDataDispatcher({ type: loginStateTypes.SET_PASSWORD, payload: e.target.value })} />
+
+            <button type='submit'>Войти</button>
+        </form>
+    )
+
+}
+
+export default LoginPage
